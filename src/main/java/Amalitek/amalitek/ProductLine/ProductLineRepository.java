546880441fb56b00f8071e1590eef9a6a392c508
lys <enocklyn@yahoo.com>
2024-02-22 15:55:07 +0000

    Enock First Commit

--- src/main/java/Amalitek/amalitek/ProductLine/ProductLineRepository.java
+++ src/main/java/Amalitek/amalitek/ProductLine/ProductLineRepository.java
@@ -1,9 +1,10 @@
 package Amalitek.amalitek.ProductLine;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductLineRepository extends JpaRepository<ProductLine, Integer> {
+public interface ProductLineRepository extends JpaRepository<ProductLine,Integer> {
 
 }
--- src/main/java/Amalitek/amalitek/ProductLine/ProductLineServiceImpl.java
+++ src/main/java/Amalitek/amalitek/ProductLine/ProductLineServiceImpl.java
@@ -1,7 +1,46 @@
 package Amalitek.amalitek.ProductLine;
 
+import Amalitek.amalitek.Product.ProductRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProductLineServiceImpl {
+@RequiredArgsConstructor
+public class ProductLineServiceImpl implements ProductLineService{
+  private  final ProductLineRepository PLR;
+    @Override
+    public ProductLine saveProductLine(ProductLine productLine) {
+       try{
+           return PLR.save(productLine);
+
+       } catch (Exception ex){
+    return null;
+
+       }
+    }
+
+    @Override
+    public ProductLine EditProductLine(ProductLine productLine) {
+       try{
+           if (PLR.findById(productLine.getId()).isPresent()){
+               return PLR.save(productLine);
+
+           }else{
+
+               return null;
+           }
+
+       }catch (Exception ex){
+       System.out.println(ex.toString());
+           return null;
+       }
+    }
+
+    @Override
+    public List<ProductLine> productLines() {
+        return null;
+    }
 }
